f351c7174eea757cc832b6af9a8c89b6d1c26f3f
Bojin Yao <bojinyao@berkeley.edu>
2019-10-03 21:35:58 -0700

    much cleaning file handling though library methods.

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from classes import *
 
 #------------------------ Flags ----------------------------
 parser = argparse.ArgumentParser(description='Process function composition program')
-parser.add_argument('path', type=str, help='Absolute path to output html file')
-parser.add_argument('name', type=str, help='file name to output content, create file if necessary (recommend .html)')
+parser.add_argument('-o', "--outs", type=argparse.FileType('w+', encoding='UTF-8'), help='Path to file for output. Default output is stdout.')
 parser.add_argument('-num_functions', type=int, default=3, help='number of functions to have as answer, must be at least 1. Default 3')
 parser.add_argument('-num_choices', type=int, default=5, help='number of choices to present to the user, 1 < num choice <= factorial(num functions). Default 5')
 parser.add_argument('-q', '--questions', type=int, default=1, help='number of questions to generate in file, default is 1. Must be no greater than factorial(num functions)')
@@ -24,26 +23,17 @@ def main():
     if args.num_choices <= 1 or args.num_choices > math.factorial(args.num_functions):
         help_and_exit()
 
-    dir_path = args.path
-    if not path.isabs(dir_path):
-        help_and_exit()
-
-    file_path = path.join(args.path, args.name)
-
-    try:
-        fp = open(file_path, 'w+')
-    except OSError:
-        fp = None
-        print(f"Cannot output file to {file_path}")
-        exit(1)
-	
+    if args.o:
+        fp = args.o
+    else:
+        fp = sys.stdout
     # composer = function_composition(fn_list) # TODO: create a functions list
     # writer = file_writer(fp, composer, args.num_functions, args.num_choices, args.questions)
     # writer.dump()
     fp.close()
     exit(0)
 
-def help_and_exit(code=1):
+def help_and_exit(code=2):
     s = '''\
         ----------------------
         Invalid Input Occurred
