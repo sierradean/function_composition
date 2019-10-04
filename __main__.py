1bdd0b283f2d35569fe800d5c1544d9111db621f
Bojin Yao <bojinyao@berkeley.edu>
2019-10-03 21:58:49 -0700

    made the entire thing to be a module that can be called from a directory level or zipped up

--- __main__.py
+++ __main__.py
@@ -6,22 +6,24 @@ import textwrap
 from classes import *
 
 #------------------------ Flags ----------------------------
-parser = argparse.ArgumentParser(description='Process function composition program')
-parser.add_argument('-o', "--outs", type=argparse.FileType('w+', encoding='UTF-8'), help='Path to file for output, create file if necessary. Default output is stdout.')
-parser.add_argument('-num_functions', type=int, default=3, help='number of functions to have as answer, must be at least 1. Default 3')
-parser.add_argument('-num_choices', type=int, default=5, help='number of choices to present to the user, 1 < num choice <= factorial(num functions). Default 5')
-parser.add_argument('-q', '--questions', type=int, default=1, help='number of questions to generate in file, default is 1. Must be no greater than factorial(num functions)')
-args = parser.parse_args()
+
 
 #------------------------ Functions ------------------------
 
 
 #------------------------ Main -----------------------------
 def main():
+    parser = argparse.ArgumentParser(description='Process function composition program')
+    parser.add_argument('-o', "--outs", type=argparse.FileType('w+', encoding='UTF-8'), help='Path to file for output, create file if necessary. Default output is stdout.')
+    parser.add_argument('-num_functions', type=int, default=3, help='number of functions to have as answer, must be at least 1. Default 3')
+    parser.add_argument('-num_choices', type=int, default=5, help='number of choices to present to the user, 1 < num choice <= factorial(num functions). Default 5')
+    parser.add_argument('-q', '--questions', type=int, default=1, help='number of questions to generate in file, default is 1. Must be no greater than factorial(num functions)')
+    args = parser.parse_args()
+
     if args.num_functions < 1 or args.questions < 1:
-        help_and_exit()
+        help_and_exit(parser)
     if args.num_choices <= 1 or args.num_choices > math.factorial(args.num_functions):
-        help_and_exit()
+        help_and_exit(parser)
 
     if args.o:
         fp = args.o
@@ -33,7 +35,7 @@ def main():
     fp.close()
     exit(0)
 
-def help_and_exit(code=2):
+def help_and_exit(parser, code=2):
     s = '''\
         ----------------------
         Invalid Input Occurred
@@ -45,4 +47,4 @@ def help_and_exit(code=2):
 
 
 if __name__ == '__main__':
-	main()
+    main()
